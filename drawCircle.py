03032863ddca1a214577cd89c449fd1f5e5d88cc
Esther Weidauer <esther@selfawaresoup.com>
2018-03-21 16:09:41 +0100

    could be faster … maybe

--- drawCircle.py
+++ drawCircle.py
@@ -1,6 +1,6 @@
 import pyb
 import lcd160cr
-import math
+from math import sqrt # so we don't need to resolve math.sqrt on every loop iteration later
 
 lcd = lcd160cr.LCD160CR('X')
 
@@ -9,17 +9,14 @@ def DrawCircle(r, dx, dy):
         dx and dy are offset in x and y
         so the circle's center is not at 0,0"""
 
-    lcd.dot(dx+r, dy+r)
-    for x in range(-r, r+1, 1):
-        z = math.sqrt(r**2-x**2)
-        y = -math.sqrt(r**2-x**2)
-        z = round(z)
-        y = round(y)
-        print (x, z)
-        print (x, y)
+    for x in range(0, r):
+        y = round(sqrt(r**2-x**2)) # do expensive operations only once
 
-        lcd.dot(x+r+dx, z+r+dy)
-        lcd.dot(x+r+dx, y+r+dy)
+        # but addition is cheap:
+        lcd.dot(dx + x, dy + y)
+        lcd.dot(dx + x, dy - y)
+        lcd.dot(dx - x, dy + y)
+        lcd.dot(dx - x, dy - y)
 
 lcd.set_pen(lcd.rgb(255, 0, 0), lcd.rgb(64, 64, 128))
 lcd.erase()
