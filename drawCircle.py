6ada5a3b044d90eb4ecd714e16bcdb779d3f55fd
Mirabellensaft <tanja@transfeld.net>
2018-03-21 19:22:39 +0100

    implemented midpoint circle algorithm

--- drawCircle.py
+++ drawCircle.py
@@ -1,28 +1,33 @@
-import pyb
-import lcd160cr
+#import pyb
+#import lcd160cr
 from math import sqrt # so we don't need to resolve math.sqrt on every loop iteration later
 
-lcd = lcd160cr.LCD160CR('X')
+#lcd = lcd160cr.LCD160CR('X')
 
 def DrawCircle(r, dx, dy):
     """r = radius of the circle
         dx and dy are offset in x and y
         so the circle's center is not at 0,0"""
 
-    for x in range(0, r):
-        y = round(sqrt(r**2-x**2)) # do expensive operations only once
+    def DrawCircle(r, dx, dy):
+        """r = radius of the circle
+            dx and dy are offset in x and y
+            so the circle's center is not at 0,0"""
 
-        # but addition is cheap:
-        lcd.dot(dx + x, dy + y)
-        lcd.dot(dx + x, dy - y)
-        lcd.dot(dx - x, dy + y)
-        lcd.dot(dx - x, dy - y)
+        for x in range(0, r):
+            if x/(sqrt(r**2-x**2)) < 1:
+                y = round(sqrt(r**2-x**2))
 
-        lcd.dot(dx + y, dy + x)
-        lcd.dot(dx - y, dy + x)
-        lcd.dot(dx + y, dy - x)
-        lcd.dot(dx - y, dy - x)
+                lcd.dot(dx + x, dy - y) #1
+                lcd.dot(dx + y, dy - x) #2
+                lcd.dot(dx + y, dy + x) #3
+                lcd.dot(dx + x, dy + y) #4
+                lcd.dot(dx - x, dy + y) #5
+                lcd.dot(dx - y, dy + x) #6
+                lcd.dot(dx - y, dy - x) #7
+                lcd.dot(dx - x, dy - y) #8
 
-lcd.set_pen(lcd.rgb(255, 0, 0), lcd.rgb(64, 64, 128))
-lcd.erase()
-DrawCircle(10, 20, 20)
+
+# lcd.set_pen(lcd.rgb(255, 0, 0), lcd.rgb(64, 64, 128))
+# lcd.erase()
+DrawCircle(30, 50, 60)
