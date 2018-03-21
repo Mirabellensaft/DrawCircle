0f388ddc523034267b6e110e03f14c24f8da5b41
Mirabellensaft <tanja@transfeld.net>
2018-03-21 14:31:57 +0100

    added parameters and offset in x and y

--- drawCircle.py
+++ drawCircle.py
@@ -1,16 +1,26 @@
+import pyb
+import lcd160cr
 import math
 
-def DrawCircle():
-    # lcd.set_pen(lcd.rgb(255, 0, 0), lcd.rgb(64, 64, 128))
-    # lcd.erase()
-    a = 10
-    b = 10
-    for x in range(-10, 10, 1):
-        z = math.sqrt(100-x**2)
-        y = -math.sqrt(100-x**2)
+lcd = lcd160cr.LCD160CR('X')
+
+def DrawCircle(r, dx, dy):
+    """r = radius of the circle
+        dx and dy are offset in x and y
+        so the circle's center is not at 0,0"""
+
+    lcd.dot(dx+r, dy+r)
+    for x in range(-r, r+1, 1):
+        z = math.sqrt(r**2-x**2)
+        y = -math.sqrt(r**2-x**2)
         z = round(z)
         y = round(y)
-        print (x+a, z+b)
-        print (x+a, y+b)
+        print (x, z)
+        print (x, y)
 
+        lcd.dot(x+r+dx, z+r+dy)
+        lcd.dot(x+r+dx, y+r+dy)
+
+lcd.set_pen(lcd.rgb(255, 0, 0), lcd.rgb(64, 64, 128))
+lcd.erase()
 DrawCircle()
